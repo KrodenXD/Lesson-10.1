155075d6b3f43ea0a6a81704d51b13db2ff24e37
alexc <alexc@SONY-VAIO>
2023-03-21 22:38:55 +0400

    Add project files.

---
+++ Lesson 10.1/Methods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10._1
+{
+    class Methods
+    {
+        public Methods()
+        {
+            
+        }
+        public Methods(int u)
+        {
+            t = u;
+        }
+        public int t;
+        public const string Address = "Abovyan-1";
+        public const string Number = "+37477777777";
+        
+        public decimal MakeFood(Menu menu,int count)
+        {
+            switch (menu)
+            {
+                case Menu.Hotdog:
+                    return (decimal)Menu.Hotdog * (count+t);
+                case Menu.Shushi:
+                    return (decimal)Menu.Shushi * (count + t);
+                case Menu.Pizza:
+                    return (decimal)Menu.Pizza * (count + t);
+                case Menu.Shawrma:
+                    return (decimal)Menu.Shawrma * (count + t);
+                case Menu.Xash:
+                    return (decimal)Menu.Xash * (count + t);
+                default:
+                    return 0;
+            }
+        }
+    }
+    public enum Menu
+    {
+        Hotdog = 500,
+        Shushi = 12000,
+        Pizza = 300,
+        Shawrma = 700,
+        Xash = 4000
+    }
+}
---
+++ Lesson 10.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            Methods methods1 = new Methods(66);
+
+            Console.WriteLine(methods.t);
+            Console.WriteLine(methods.MakeFood(Menu.Pizza,6));
+        }
+
+        
+    }
+}
